a97b764ec78bcb62af2715a11e5809b03873d118
Zachary Schulz <zjschulz@gmail.com>
2019-08-30 23:32:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
     :reading_railroad => {},
     :pennsylvania_railroad => {},
     :b_and_o_railroad => {},
-    :shortline => {}
+    :shortline_railroad => {}
   }
   }
   }
