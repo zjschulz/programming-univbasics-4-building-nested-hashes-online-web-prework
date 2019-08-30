4d9184aa2be8183e628361af1840459be7327512
Zachary Schulz <zjschulz@gmail.com>
2019-08-30 23:40:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,6 @@ def monopoly_with_fourth_tier
   }
   }
   }
-  puts monopoly[]
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
